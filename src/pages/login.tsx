978415366098ce103a5ae9ba761e4346c9950cb7
andredavid1 <andredavid1@yahoo.com.br>
2022-01-17 20:08:49 -0300

    refactor: delete loading in login page

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,11 +1,8 @@
 import FormLogin from "components/FormLogin";
 import LayoutLogin from "layouts/Login";
 import { NextPage } from "next";
-import useAuth from "../hooks/useAuth";
 
 const Login: NextPage = () => {
-  const {loading} = useAuth();
-
   return (
     <LayoutLogin>
       <FormLogin />
